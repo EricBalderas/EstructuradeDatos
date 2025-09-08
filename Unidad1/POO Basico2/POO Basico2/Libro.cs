29cf600bb0a7afbd31bab5b467419d01b1de4ede
EricBalderas <ericflore197@gmail.com>
2025-09-07 19:00:49 -0500

    Se agrego el programa POO basico 2

---
+++ Unidad1/POO Basico2/POO Basico2/Libro.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Basico2
+{
+    internal class Libro
+    {
+		private string strTitulo;
+
+		public string Titulo
+		{
+			get { return strTitulo; }
+			set { strTitulo = value; }
+		}
+		private string strAutor;
+
+		public string Autor
+		{
+			get { return strAutor;; }
+			set { strAutor = value; }
+		}
+
+		public void InfoLibro ()
+		{
+			Console.WriteLine("Libro: " + Titulo +  "\nAutor: " + Autor);
+        }
+
+    }
+}
---
+++ Unidad1/POO Basico2/POO Basico2/Program.cs
@@ -0,0 +1,12 @@
+﻿using POO_Basico2;
+
+Libro miLibro = new Libro();
+miLibro.Titulo = "";
+miLibro.Autor = "";
+
+Console.Write("Ingrese el titulo del libro: ");
+miLibro.Titulo = Console.ReadLine();
+Console.Write("Ingrese el autor del libro: ");
+miLibro.Autor = Console.ReadLine();
+
+miLibro.InfoLibro();
